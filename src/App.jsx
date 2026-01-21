b92f88e110543ff0c5d6c7956d89b1ae931e05af
Jesús Aldoman <chus.ao.2005@gmail.com>
2026-01-21 18:47:04 +0100

    Haciendo el nav, primer paso

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 export default function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <nav className="w-full bg-slate-900">
+        <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+
+          <div className="text-xl font-bold text-white">
+            AniMedar
+          </div>
+
+          <div className="flex gap-4">
+            <button className="text-slate-300 hover:text-white transition">
+              Iniciar sesión
+            </button>
+
+            <button className="px-5 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition">
+              Registrarse
+            </button>
+          </div>
+
+        </div>
+      </nav>
+
+
 
     </>
   )
